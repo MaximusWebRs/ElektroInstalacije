0b8fc91e703fd3d4641dc55775b10d94a4d99024
Maximus <zagoracnikola.maximus@gmail.com>
2024-08-14 15:17:06 +0200

    form

--- js/mail-sender.js
+++ js/mail-sender.js
@@ -1,16 +1,29 @@
 function sendMail() {
-    alert("sending");
-    let params = {
-        from_name: "test",
-        from_mail: document.getElementById("contact-email").value,
-        message: "testtttt",
-    }
-    alert("sending 2");
-    emailjs.send("service_elektro", "template_elektro", params).then(
+
+
+    
+    emailjs.send("service_elektro", "template_elektro", gatherParameters()).then(
         (response) => {
-          alert('SUCCESS!');
+          alert('Upit je uspešno poslat. Hvala Vam!');
         },
         (error) => {
-          alert(error);
+          alert("Upit nije poslat. Molimo Vas da probate ponovo kasnije ili da nas kontaktirate direktno.");
         });
+}
+
+function gatherParameters() {
+    let checkedServices = document.querySelectorAll("input[name='extra']:checked");
+    let theServices;
+    for (let i = 0; i < checkedServices.length; i++) {
+        theServices[i] = checkedServices[i].value;
+    }
+
+    let params = {
+        from_name: document.getElementById("form-name").value,
+        from_mail: document.getElementById("form-email").value,
+        from_number: document.getElementById("form-phone").value,
+        object_type: document.querySelector("input[name='offer']:checked").value,
+        services: theServices
+    };
+    return params;
 }
